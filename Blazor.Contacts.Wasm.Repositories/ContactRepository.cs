45408e7cc7eb10b762ca078fd255ca59ecdb91a6
alejo190404 <alejo190404@gmail.com>
2023-11-27 12:28:19 -0500

    First commit

---
+++ Blazor.Contacts.Wasm.Repositories/ContactRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Contacts.Wasm.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public ContactRepository(IDbConnection dbConnection) {
+            _dbConnection = dbConnection;
+        }
+    }
+}
---
+++ Blazor.Contacts.Wasm/Server/Program.cs
@@ -0,0 +1,48 @@
+using Blazor.Contacts.Wasm.Client.Services;
+using Blazor.Contacts.Wasm.Repositories;
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Data;
+using System.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactService, ContactService>();
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+
+string dbConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddSingleton<IDbConnection>((sp) => new SqlConnection());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
