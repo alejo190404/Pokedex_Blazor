2b891a272a1caa445d79aef4da45244ee001d7dd
alejo190404 <alejo190404@gmail.com>
2023-11-30 18:42:35 -0500

    Modelado de pokemones simplificado e implementación de métodos en el servicio (get y getAll)

---
+++ BlazorPokedex.Models/Result.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Models
+{
+    public class Result
+    {
+        [JsonProperty("results")]
+        List<Pokemon> Pokemons { get; set; }
+    }
+}
---
+++ BlazorPokedex.Services/PokeApiClient.cs
@@ -0,0 +1,29 @@
+﻿using BlazorPokedex.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Services
+{
+    public class PokeApiClient : IPokeApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public PokeApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Pokemon>> GetAllPokemons()
+        {
+            await _httpClient.GetStringAsync($"pokemon?limit=24&offset=24");
+        }
+
+        public async Task<Pokemon> GetPokemon(string name)
+        {
+            return JsonConvert.DeserializeObject<Pokemon>(await _httpClient.GetStringAsync($"pokemon/{name}"));
+        }
+    }
+}
